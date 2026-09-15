ed843dd7d7d86e28aa457d67f5569502b7118dbb
BounceBruhni <114104512+BounceBruhni@users.noreply.github.com>
2026-09-15 02:33:00 +0200

    Added new static Methods DivideComplexInt and PolarToCartesianInt. Added new Instance-Methods CartesianToPolarInt,Scale and ComplexReciprocal. Also edited the Readme.md

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JComplex
-Provides Complex Numbers for Java
+Provides Complex Numbers for Java and aims to give Access 
+to a robust and tested Set of Tools for their Usage in Production.
 Parts stem from "https://introcs.cs.princeton.edu/java/97data/Complex.java.html"
--- src/ComplexInt.java
+++ src/ComplexInt.java
@@ -15,7 +15,7 @@ public class ComplexInt {
         this.real = 0;
         this.imaginary = 0;
     }
-    //Methods:
+    //Instance-Methods:
     public int ComplexReal() {
         return real;
     }
@@ -27,6 +27,31 @@ public class ComplexInt {
         return new int[] {real, imaginary};
     }
     public ComplexInt ComplexConjugate() { return new ComplexInt(real, -imaginary);}
+    public int Scale() {
+        return (int) Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imaginary, 2));}
+    //ComplexReciprocal can be called by an Instance of ComplexInt x and returns 1/x
+    public ComplexInt ComplexReciprocal() {
+        return new ComplexInt(this.real / this.Scale(), -this.imaginary / this.Scale());
+    }
+    public int[] CartesianToPolarInt() {
+        //when is ComplexNumber < 0?
+        boolean re = this.real < 0;
+        boolean im = this.imaginary < 0;
+        int alpha = 0;
+        if (re && im) {
+            alpha = (int) ((int) Math.atan(this.imaginary / this.real) + Math.PI);}
+        else {
+            alpha = (int) Math.atan(this.imaginary / this.real);}
+
+        return new int[] {this.Scale(), alpha};
+    }
+    //static Methods:
+    public static ComplexInt PolarToCartesianInt(int[] polar) throws IllegalArgumentException {
+        if (polar.length != 2) {
+            throw new IllegalArgumentException();}
+        else {
+            return new ComplexInt((int)(polar[0]*Math.cos(polar[1])), (int)Math.sin(polar[1]));}
+    }
     /*takes 2 ComplexInt-Objects, each in the form a +ib, as an Input*/
     public static ComplexInt AddComplexInt(ComplexInt number1, ComplexInt number2) {
         return new ComplexInt((number1.real + number2.real), (number1.imaginary + number2.imaginary));
@@ -38,7 +63,7 @@ public class ComplexInt {
         return new ComplexInt((number1.real*number2.real-number1.imaginary*number2.imaginary),
                 (number1.real* number2.imaginary+ number2.real* number1.imaginary));
     }
-    public static ComplexInt DivideComplexInt(ComplexInt number1, ComplexInt number2) {
-        return new ComplexInt();
+    public static ComplexInt DivideComplexInt(ComplexInt dividend, ComplexInt divisor) {
+        return MultiplyComplexInt(dividend, divisor.ComplexReciprocal());
     }
 }
